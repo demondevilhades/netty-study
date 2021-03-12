c23f03c964e10acb61cf34b5872359e94d5aee19
demondevilhades <demonhades@foxmail.com>
2021-03-12 16:58:48 +0800

    nio server & client

---
+++ src/main/java/test/netty/base/nio/ClientTest.java
@@ -0,0 +1,50 @@
+package test.netty.base.nio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ClientTest {
+
+    public void run() {
+        try (SocketChannel socketChannel = SocketChannel.open();) {
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(ServerTest.port);
+            socketChannel.configureBlocking(false);
+            if (!socketChannel.connect(inetSocketAddress)) {
+                do {
+                    log.info("connecting...");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                } while (!socketChannel.finishConnect());
+            }
+            log.info("connected");
+            try (InputStreamReader isr = new InputStreamReader(System.in);
+                    BufferedReader br = new BufferedReader(isr);) {
+                do {
+                    String line = br.readLine();
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(line.getBytes());
+                    socketChannel.write(byteBuffer);
+                    log.info("send : {}", line);
+                    if("stop".equals(line)) {
+                        log.info("stop");
+                        break;
+                    }
+                } while (socketChannel.isConnected());
+            }
+        } catch (IOException e) {
+            log.info("", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ClientTest().run();
+    }
+}
---
+++ src/main/java/test/netty/base/nio/ServerTest.java
@@ -0,0 +1,69 @@
+package test.netty.base.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ServerTest {
+    static final int port = 9527;
+
+    public void run() {
+        final AtomicBoolean running = new AtomicBoolean(true);
+        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+                Selector selector = Selector.open();) {
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+            serverSocketChannel.socket().bind(inetSocketAddress);
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            log.info("start");
+
+            while (running.get()) {
+                log.info("checking...");
+                if (selector.select(5000) > 0) {
+                    log.info("accept key");
+                    Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
+                    while (keyIterator.hasNext()) {
+                        SelectionKey selectionKey = keyIterator.next();
+                        log.info("key : hashCode = {}", selectionKey.hashCode());
+                        if(selectionKey.isAcceptable()) {
+                            SocketChannel socketChannel = serverSocketChannel.accept();
+                            socketChannel.configureBlocking(false);
+                            socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                            log.info("accept channel : hashCode = {}", socketChannel.hashCode());
+                        } else if (selectionKey.isConnectable()) {
+                        } else if (selectionKey.isReadable()) {
+                            SocketChannel socketChannel = (SocketChannel)selectionKey.channel();
+                            ByteBuffer byteBuffer = (ByteBuffer) selectionKey.attachment();
+                            int read = socketChannel.read(byteBuffer);
+                            log.info("position = {}, limit = {}, capacity = {}", byteBuffer.position(), byteBuffer.limit(), byteBuffer.capacity());
+                            String str = new String(byteBuffer.array(), 0, read);
+                            byteBuffer.clear();
+                            log.info("read[{}] = {}", socketChannel.hashCode(), str);
+                            if("stop".equals(str)) {
+                                log.info("stop");
+                                running.set(false);
+                            }
+                        } else if (selectionKey.isWritable()) {
+                        }
+                        keyIterator.remove();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            log.info("", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ServerTest().run();
+    }
+}
