90f85d079abebea51ca9bb42fd1fea38f76e7b71
demondevilhades <demonhades@foxmail.com>
2021-08-31 15:31:44 +0800

    update replaying

---
+++ src/main/java/test/netty/replay/Client.java
@@ -0,0 +1,83 @@
+package test.netty.replay;
+
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.MessageToByteEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author awesome
+ */
+@Slf4j
+public class Client {
+
+    /**
+     * 
+     */
+    public void run() {
+        NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast("Encoder", new MessageToByteEncoder<Long>() {
+
+                                        @Override
+                                        protected void encode(ChannelHandlerContext ctx, Long msg, ByteBuf out)
+                                                throws Exception {
+                                            out.writeLong(msg);
+                                        }
+                                    })
+                                    .addLast("test", new SimpleChannelInboundHandler<Long>() {
+
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                            ctx.writeAndFlush(123456L);
+                                        }
+
+                                        @Override
+                                        protected void channelRead0(ChannelHandlerContext ctx, Long msg)
+                                                throws Exception {
+                                        }
+                                    });
+                        }
+                    });
+            ChannelFuture cf = bootstrap.connect(new InetSocketAddress("127.0.0.1", Server.PORT)).sync();
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if ("exit".equalsIgnoreCase(line.trim())) {
+                        break;
+                    }
+                    cf.channel().writeAndFlush(line);
+                }
+            } catch (Exception e) {
+                log.error("", e);
+            }
+            cf.channel().closeFuture();
+        } catch (InterruptedException e) {
+            log.error("", e);
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client().run();
+    }
+}
---
+++ src/main/java/test/netty/replay/Server.java
@@ -0,0 +1,85 @@
+package test.netty.replay;
+
+import java.util.List;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.ReplayingDecoder;
+import io.netty.util.concurrent.GenericFutureListener;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author awesome
+ */
+@Slf4j
+public class Server {
+    static final int PORT = 9527;
+
+    /**
+     * 
+     */
+    public void run() {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        try {
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            log.info("SocketChannel.is = {}", ch.id());
+                            
+                            ch.pipeline()
+                                    .addLast("Decoder", new ReplayingDecoder<Void>() {
+                                        
+                                        @Override
+                                        protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+                                            out.add(in.readLong());
+                                        }
+                                    })
+                                    .addLast("test", new SimpleChannelInboundHandler<Long>() {
+
+                                        @Override
+                                        protected void channelRead0(ChannelHandlerContext ctx, Long msg)
+                                                throws Exception {
+                                            log.info("msg = {}", msg);
+                                        }
+                                    });
+                        }
+                    });
+            ChannelFuture cf = bootstrap.bind(PORT).addListener(new GenericFutureListener<ChannelFuture>() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if(future.isSuccess()) {
+                        log.info("future is success");
+                    } else {
+                        log.info("future is not success");
+                    }
+                }
+            }).sync();
+            cf.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("", e);
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server().run();
+    }
+}
